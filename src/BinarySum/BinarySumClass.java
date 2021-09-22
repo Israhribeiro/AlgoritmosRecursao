4ac7c44f1d7676175b287d7c9f3b405c2959c87f
Israel Tavares Ribeiro <39707610+Israhribeiro@users.noreply.github.com>
2021-09-21 22:13:24 -0300

    BinaryFibonacci

--- src/BinarySum/BinarySumClass.java
+++ src/BinarySum/BinarySumClass.java
@@ -1,16 +1,21 @@
 package BinarySum;
 
 public class BinarySumClass {
-    public static int BinarySum(int[] A, int i, int n) {
-        if(n == 1){
-            return A[i];
-        }
-        return BinarySum(A, i, (int) Math.ceil(n/2)) + BinarySum(A, i + (int) Math.ceil(n/2), (int) Math.floor(n/2));
+    public static int binarySum(int[] data, int low, int high){
+
+            if(low > high)
+                return 0;
+            else if(low == high)
+                return data[low] ;// there is only one element in the array
+            else {
+                int mid = (low + high)/2;
+                return binarySum(data, low, mid) + binarySum(data, mid+1, high);
+            }
     }
 
     public static void main(String[] args){
         int[] A = new int[] {1,2,3,4,5};
-        System.out.println(BinarySum(A,0,5));
+        System.out.println(binarySum(A,0,5));
     }
 
 }
